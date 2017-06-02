d5eadb204dea6c5185b83e8a4997c174e1f6ddec
Lemuel Recopuerto <lemuelrecopuerto@gmail.com>
2017-06-02 13:52:30 +0800

    Added Welcome Page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,10 +5,10 @@
     <div class="row">
         <div class="col-md-10 col-md-offset-1">
             <div class="panel panel-default">
-                <div class="panel-heading">Welcome</div>
+                <div class="panel-heading">Welcome to PMS!</div>
 
                 <div class="panel-body">
-                    Your Application's Landing Page.
+                    <h1>Project Monitoring System using Laravel</h1>
                 </div>
             </div>
         </div>
